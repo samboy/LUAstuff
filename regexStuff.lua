86bdf32946c183a6ce9e0d05fee996af866962c2
Sam Trenholme <strenholme.usenet@gmail.com>
2020-07-14 23:25:07 -0700

    regexStuff: Add rStrSplit method (CSV/AWK like splitter)

--- regexStuff.lua
+++ regexStuff.lua
@@ -3,6 +3,11 @@
 -- Output: An array (numeric indexed table) with the split string
 -- Should the character we split on not be in the string to split, 
 -- we will have a one-element array with the input string
+--
+-- Note: Should the splitter regex be a single character, things will 
+-- split as expected, but 0-length strings are converted in to a 
+-- one-character long space (" ") string.
+--
 -- Example usage:
 -- a = "1,2,3" t = rSplit(a,",") for k,v in pairs(t) do print(k,v) end
 function rCharSplit(i, c)
@@ -22,3 +27,24 @@ function rCharSplit(i, c)
   end
   return out
 end 
+
+----------------------- rStrSplit() -----------------------
+-- This does a simple split for a given string
+-- Input: string, split character 
+--
+function rStrSplit(s, splitOn)
+  if not splitOn then splitOn = "," end
+  local place = 1
+  local out = {} 
+  local last = 1
+  while place do
+    place = string.find(s, splitOn, place, true) 
+    if place then
+      table.insert(out,string.sub(s, last, place - 1))
+      place = place + 1
+      last = place
+    end
+  end 
+  table.insert(out,string.sub(s, last, -1))
+  return out
+end
--- regexStuffTest.lua
+++ regexStuffTest.lua
@@ -7,3 +7,20 @@ print("")
 
 b="This is a big  test!"
 tablePrint(rCharSplit(b,"%s"))
+
+--- rStrSplit
+b="1,,2,3,45,Hello there!,6,789"
+print("rStrSplit test #1 ---------------------------")
+tablePrint(rStrSplit(a))
+print("rStrSplit test #2 ---------------------------")
+tablePrint(rStrSplit(b))
+print("rStrSplit test #3 ---------------------------")
+for i,v in ipairs(rStrSplit(a)) do
+  print(v,string.len(v))
+end
+-- In comparison, this will not have a 0 len string
+print("---------------------------------------------")
+for i,v in ipairs(rCharSplit(a,",")) do
+  print(v,string.len(v))
+end
+

c89b6d5effbfacc4ed8132d44b5fdf39bca8e49b
Sam Trenholme <git@samiam.org>
2020-12-06 17:05:16 -0800

    inauguration.lua: Countdown to Biden’s inauguration

---
+++ inauguration.lua
@@ -0,0 +1,20 @@
+#!/usr/bin/env lua
+
+-- This will currently *not* work with Lunacy, since it uses os.time()
+
+now = os.time()
+count = 1611162000 - now
+if count < 0 then
+  print "Joe Biden has been inaugurated president"
+  os.exit(0)
+end
+days = math.floor(count / 86400)
+hours = math.floor((count - (days * 86400)) / 3600)
+minutes = math.floor((count - (days * 86400) - (hours * 3600)) / 60)
+seconds = count - (days * 86400) - (hours * 3600) - (minutes * 60)
+
+print("Joe Biden will be inaugurated president in ")
+print(tostring(days) ..
+      " days, " .. tostring(hours) .. " hours, " .. tostring(minutes) ..
+      " minutes, " .. tostring(seconds) .. " seconds.")
+

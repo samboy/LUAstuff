f29a7d8645b4195c2e5ec232c1f304079b1d1161
Sam Trenholme <git@samiam.org>
2020-12-07 21:53:09 -0800

    logtable.lua: Add “Live 105” mode
    The live 105 mode adds an extra column with the logs for 1.05, 1.15,
    1.25, 1.35, and so on.  As per Benford’s law, it makes for sense to
    have more numbers closer to 1 if we want to give the table some more
    precision while keeping it small enough to fit on a coffee mug.

--- logtable.lua
+++ logtable.lua
@@ -2,15 +2,28 @@
 
 -- Print a common log table suitable for a coffee mug
 
-function l(a) return math.log(a)/math.log(10) end
-function m(a) return string.format("%.4f",l(a)):gsub("^0","\t") end
+local live105 = false
+if #arg >= 1 then
+  if arg[1] == '-105' then live105 = true end
+end
+
+local function l(a) return math.log(a)/math.log(10) end
+local function m(a) return string.format("%.4f",l(a)):gsub("^0","\t") end
 local z = ""
 for a=1,9 do
   z = z .. "\t" .. tostring(a)
+  if live105 and a == 1 then
+    z = z .. "\t1.05"
+  end
 end
 print(z) 
 for a=0,.9,.1 do
-  print(tostring(a):gsub("^0","")..m(1+a)..m(2+a)..m(3+a)..m(4+a)..
-        m(5+a)..m(6+a)..m(7+a)..m(8+a)..m(9+a))
+  local line = tostring(a):gsub("^0","")..m(1+a)
+  if live105 then
+    line = line .. m(1.05 + a)
+  end
+  line = line .. m(2+a)..m(3+a)..m(4+a)
+  line = line .. m(5+a)..m(6+a)..m(7+a)..m(8+a)..m(9+a)
+  print(line)
 end
 
